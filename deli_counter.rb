acd1f8f5e5bf3799a01df1ac9ac1216246f1aa51
Jason Markson <jasonmarkson76@gmail.com>
2020-08-25 23:44:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number (katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   katz_deli.shift(name)
   puts "Currently serving #{name}."
   
