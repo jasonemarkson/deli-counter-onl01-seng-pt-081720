73dab095111d29438da808618e5c19136064afab
Jason Markson <jasonmarkson76@gmail.com>
2020-08-25 23:48:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def take_a_number (katz_deli, name)
 end
 
 def now_serving(katz_deli)
+  katz_deli.shift
+  puts "Currently serving #{katz_deli[0]"
   puts "There is nobody waiting to be served!"
-  katz_deli.shift()
 end
