2db40d5d8fa212fb1b2ee1533ba262157a96eeca
Jason Markson <jasonmarkson76@gmail.com>
2020-08-25 23:04:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line (katz_deli)
+  if katz_deli.length >= 1 
+    new_line = []
+    counter = 1 
+    katz_deli.each do |name|
+      new_line.push("#{counter}. #{name}")
+      counter += 1 
+    end
+  puts "The line is currently: #{new_line.join(" ")}"
+  else 
+    puts "The line is currently empty."
+  end
+end
+
+line(katz_deli)
+
+def take_a_number(katz_deli, name)
+  if katz_deli >= 1 
+  katz_deli.each_with_index do |name, element|
+    puts katz_deli.push("Welcome, #{name}. You are number #{element + 1} in line.")
+  end
+  else 
+    puts katz_deli.push(name)
+  end
+end
