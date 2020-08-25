c7ff32d1e627f254ce6b5a3feb28e6b09b3f234c
Jason Markson <jasonmarkson76@gmail.com>
2020-08-25 23:48:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,6 @@ end
 
 def now_serving(katz_deli)
   katz_deli.shift
-  puts "Currently serving #{katz_deli[0]"
+  puts "Currently serving #{katz_deli[0]}"
   puts "There is nobody waiting to be served!"
 end
