e88c562bab1bd94c6def395509eb36a7a8424241
Jason Markson <jasonmarkson76@gmail.com>
2020-08-26 00:34:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number (katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli)
+def now_serving(katz_deli, name)
   if line.length == 0 
     puts "There is nobody waiting to be served!"
   else
